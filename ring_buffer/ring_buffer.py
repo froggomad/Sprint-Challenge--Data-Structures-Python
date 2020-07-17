79bc6b24c828f835e292bd518bb9ad5a3f5a12f6
Kenny Dubroff <kenny.dubroff@kennydubroff.com>
2020-07-17 09:04:53 -0700

    solved ring buffer using list

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,32 @@
 class RingBuffer:
-    def __init__(self, capacity):
-        pass
-
-    def append(self, item):
-        pass
+    def __init__(self, max_value):
+        self.pointer = 0
+        self.max_value = max_value
+        self.storage = []
 
     def get(self):
-        pass
+        return self.storage
+
+    def append(self, value):
+        if len(self.storage) < self.max_value:
+            self.storage.append(value)
+        else:
+            if self.pointer == self.max_value:
+                self.pointer = 0
+            self.storage[self.pointer] = value
+            self.pointer += 1
+
+
+buffer = RingBuffer(3)
+
+buffer.append('a')
+buffer.append('b')
+buffer.append('c')
+
+print(buffer.get())
+
+buffer.append('d')
+buffer.append('e')
+buffer.append('f')
+
+print(buffer.get())
